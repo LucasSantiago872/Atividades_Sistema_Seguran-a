9f9eae509bbaec7a63e10c4a0db9b713dabda7b5
LucasSantiago872 <lucas.asc08@gmail.com>
2026-05-06 18:52:32 -0300

    Update Chave_Simetrica.py

--- python/Chave_Simetrica.py
+++ python/Chave_Simetrica.py
@@ -4,16 +4,12 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import hashlib
 
-# =======================================================
-# 1. CRIPTOGRAFIA SIMÉTRICA (usa a mesma chave)
-# =======================================================
 print("=== CRIPTOGRAFIA SIMÉTRICA ===")
 
 # Gerar chave
 chave_simetrica = Fernet.generate_key()
 fernet = Fernet(chave_simetrica)
 
-# Texto original
 mensagem = "Segurança é importante!"
 
 # Criptografar
@@ -25,5 +21,3 @@ mensagem_original = fernet.decrypt(mensagem_criptografada).decode()
 print("Chave:", chave_simetrica)
 print("Mensagem criptografada:", mensagem_criptografada)
 print("Mensagem original:", mensagem_original)
-
-print("\n-------------------------------------------\n")
